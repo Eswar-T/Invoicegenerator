0952883d0c27e97f0b788040459c4dc2c68357e4
Eswar-T <t.eswarvarun@gmail.com>
2025-02-02 10:12:16 +0530

    added a title and date in pdf from xlsx

--- main.py
+++ main.py
@@ -3,16 +3,21 @@ import pandas as pd
 from fpdf import FPDF
 from pathlib import Path
 
-filepaths = glob.glob("textfiles/*.txt")
-# df = pd.read_fwf(filepath)
-pdf = FPDF(orientation="P", unit="mm", format="A4")
+filepaths = glob.glob("textfiles/*.xlsx")
+
 for filepath in filepaths:
+    df = pd.read_fwf(filepath)
+    pdf = FPDF(orientation="P", unit="mm", format="A4")
 
     pdf.add_page()
     filename = Path(filepath).stem
-    invoice_nr = filename.title()
+
+    invoice_nr, date = filename.split("-")
     pdf.set_font(family="Times",size=16,style="B")
-    pdf.cell(w=50,h=8,txt=invoice_nr)
+    pdf.cell(w=50,h=8,txt=f"invoice_nr:{invoice_nr}",ln=1)
+
+    pdf.set_font(family="Times", size=16, style="B")
+    pdf.cell(w=50, h=8, txt=f"Date:{date}")
     pdf.output(f"PDFS/{filename}.pdf")
 
 
