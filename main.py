0a10028b60546f36e30695e3881571fd8b419d70
Eswar-T <t.eswarvarun@gmail.com>
2025-02-02 06:46:31 +0530

    text files to pdf

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import glob
 import pandas as pd
+from fpdf import FPDF
+from pathlib import Path
 
-filepaths = glob.glob("Invoices/*.xlsx")
-
+filepaths = glob.glob("textfiles/*.txt")
+# df = pd.read_fwf(filepath)
+pdf = FPDF(orientation="P", unit="mm", format="A4")
 for filepath in filepaths:
-    df = pd.read_excel(filepath,sheet_name="Sheet 1")
-    print(df)
+
+    pdf.add_page()
+    filename = Path(filepath).stem
+    invoice_nr = filename.title()
+    pdf.set_font(family="Times",size=16,style="B")
+    pdf.cell(w=50,h=8,txt=invoice_nr)
+    pdf.output(f"PDFS/{filename}.pdf")
+
+
+# pdf.output("output.pdf")
